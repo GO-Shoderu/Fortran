6ec3652b58fff22dd555f476d4fb027ed876dc9c
GO-Shoderu <go.shoderu@gmail.com>
2022-10-10 22:13:29 +0200

    ✨ A simple code asking what your name is

--- s16186258.f90
+++ s16186258.f90
@@ -1,3 +1,11 @@
 program calculation
-    ! a base structure of fortran program
+    implicit none
+    
+    character (len = 20) :: f_name, l_name
+    
+    print *, "What's your name "
+    read *, f_name, l_name
+
+    print *, "Hello ", trim(f_name), " ", trim(l_name)
+
 end program calculation
