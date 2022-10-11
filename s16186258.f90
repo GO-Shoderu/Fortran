12d20c65c4f694900e4629db49225952340b2fc3
GO-Shoderu <go.shoderu@gmail.com>
2022-10-11 06:17:00 +0200

    💡 Added comments where necessary

--- s16186258.f90
+++ s16186258.f90
@@ -1,11 +1,15 @@
 program calculation
     implicit none
     
+    ! a simple program collecting user input and outputing it with a hello statement
+    ! creating variables for user input
     character (len = 20) :: f_name, l_name
     
+    ! prompting the user for their respective inputs
     print *, "What's your name "
     read *, f_name, l_name
 
+    ! outputting user input with hello presceding it
     print *, "Hello ", trim(f_name), " ", trim(l_name)
 
 end program calculation
