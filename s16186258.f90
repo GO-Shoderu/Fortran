90b211079e9f91e449b5fcfccaa1d6cd68235b13
GO-Shoderu <go.shoderu@gmail.com>
2022-10-11 06:28:38 +0200

    ✨ Added codes for formatting outputs

--- s16186258.f90
+++ s16186258.f90
@@ -6,10 +6,29 @@ program calculation
     character (len = 20) :: f_name, l_name
     
     ! prompting the user for their respective inputs
-    print *, "What's your name "
-    read *, f_name, l_name
+    print *, "What's your name?: "
+    read *, f_name, l_name 
 
     ! outputting user input with hello presceding it
     print *, "Hello ", trim(f_name), " ", trim(l_name)
+
+    ! how to output and also formating output 
+    ! for integer 
+    print *, "A number ", 10
+    print "(3i5)", 7, 6, 8
+    print "(i5)", 7, 6, 8
+
+    ! for floats 
+    print "(/, 2f8.5)", 3.1415, 1.234   !"/" is for a newline
+
+    ! for characters
+    print "(/, 2a8)", "Name", "Age"
+
+    ! exponential output
+    print "(/, e10.3)", 123.456
+
+    ! output containing strings and integer, you can use te same idea for floats as well ... 
+    ! you get the vibe ..... lol
+    print "(/, a5, i2)", "I am ", 27
 
 end program calculation
